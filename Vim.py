4f8a0b8f9b89d34ab1afb102d5a8bc22a423cdac
Yash Bhutoria <yash222219@gmail.com>
2017-08-07 14:46:42 +0530

    Made the Editor Modular and Colorful

--- Vim.py
+++ Vim.py
@@ -15,14 +15,31 @@ intrepretor.
 #imported under used libraries
 import sys 
 import os
+import time
+#following class has static charachters which enables colors when concatinated with strings 
+class color:
+	#Start with any of these
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+	#End with color.ENDC always
+	ENDC = '\033[0m'
 
+def splashScreen():
+	#splash screen for two seconds
+	print(color.HEADER + color.BOLD + color.UNDERLINE + "\t\tWelcome to Vim Mimic 1.0" + color.ENDC)
+	time.sleep(2)
 
 #To create a newfile and write Data to it.
 #To be used when file with the name is not found.
 def create(fileName):
 	f= open(fileName,'w')
 	#Debugged_by_Yash: Print Statement has to be outside the loop to avoid multiple echo
-	print("Enter Content of File , enter #exit to exit ")
+	print(color.OKBLUE+"Enter Content of File , enter #exit to exit " + color.OKGREEN)
 	while True:
 		content = input()
 		if content == '#exit':
@@ -31,49 +48,53 @@ def create(fileName):
 		f.write(content + "\n")
 	f.close()
 
+def editFile(fileName):
+	try:
+		f = open(fileName,'r+')
+		f.seek(0)
+		#while added for multiple edits
+		while(True):
+			print(color.OKBLUE+'Current Cursor Position:' + color.ENDC,f.tell())
+			change = int(input(color.BOLD +'\nEnter The Number of Characters You Want To Move The Cursor\n\tor\nEnter 0 to Finish\n: '+color.ENDC))
+			if(change == 0):
+				break
+			#Debugged_by_Yash:Folowing line added to actually move the cursor
+			f.seek(f.tell() + change)
+			
+			text = input(color.BOLD+'\nEnter new content\n: '+color.ENDC)
+			#Debugged_by_Yash: no need for if and else statements
+			f.write( text )
+			f.clear()
+			readFile(fileName)
+	except:
+		print(color.FAIL +"File To Be Read Not Found !" + color.ENDC)
 
+def readFile(fileName):
+	try:
+		f = open(fileName,'r')
+		content = f.read()
+		print(color.HEADER+"-----------------"+fileName+"-----------------"+color.ENDC)
+		print(color.OKGREEN + content + color.ENDC)
+		print(color.HEADER+"----------------------------------------------\n"+color.ENDC)
+		f.close()
+	except:
+		print(color.WARNING +"File Does Not Exist, Created New File" + color.ENDC)
+		create(fileName)	
 
 
 
 def main():
-	#Write the starter code that displays 
-	#The program name and pauses for some time.
-	#Then cleares the screen.
-
-
-	#Now the idea is to check , whether a file 
-	# with the filename as specified by user 
-	# exist or not.
-
-	try:
-		
-		#this edit section is not working yet !
-		
-		f = open(sys.argv[1],'r+')
-		# Display the content available in the 
-		# file currently.
-		content = f.read()
-		print(content)
-		# As after reading the complete file
-		# the file pointer has moved to the end 
-		# of the file , we should set it back to 
-		# starting by using seek function
-		f.seek(0)
-		
-		currentPointer = f.tell()
-		
-		print('Current filepointer Position:',currentPointer)
-		change = int(input('\nEnter the number of characters you want to move the pointer: '))
-		
-		#Debugged_by_Yash:Folowing line added to actually move the cursor
-		f.seek(f.tell() + change)
-		
-		text = input('\nEnter the content to be replaced\n: ')
-		#Debugged_by_Yash: no need for if and else statements due to line 69
-		f.write( text )
-	except:
-		create(sys.argv[1])
-
+	splashScreen()
+	fileName = sys.argv[1]
+	while(True):
+		os.system("clear")
+		readFile(fileName)
+		print(color.BOLD + color.OKBLUE+"\nPress 1 to Edit \n       or\nPress 0 to Exit"+color.ENDC)
+		opt = input()
+		if(opt != "0"):
+			editFile(fileName)
+		else:
+			break	
 
 
 if __name__ == '__main__':
